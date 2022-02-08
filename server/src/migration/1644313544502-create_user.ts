d5eba82e12fb736242c8b5ceae785335144c511a
kp20181018 <kp20181018@student.fon.bg.ac.rs>
2022-02-08 10:46:26 +0100

    Napravljena migracija za usera

---
+++ server/src/migration/1644313544502-create_user.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createUser1644313544502 implements MigrationInterface {
+    name = 'createUser1644313544502'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`firstName\` varchar(255) NOT NULL, \`lastName\` varchar(255) NOT NULL, \`age\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`user\``);
+    }
+
+}
